73f46843bdfd981c5551759c376bdc4c2d36f262
dunteyr <39315568+dunteyr@users.noreply.github.com>
2024-02-19 17:03:22 -0700

    Moving platforms and death
    Made random platforms that move down the screen and that respawn at the top when they hit the bottom. Also the player dies if he hits the bottom of the screen

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 vec = pygame.math.Vector2
 
@@ -7,6 +8,8 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, width, height, x, y):
         super().__init__()
 
+        self.isControllable = True
+        self.isDead = False
         self.collisionGroups = []
 
         self.acceleration = 0.5
@@ -26,17 +29,22 @@ class Player(pygame.sprite.Sprite):
     def move(self):
         self.acc = vec(0, 0.5)
 
-        collisions = self.getCollisions()
+        #ignore collisions if dead
+        if not self.isDead:
+            collisions = self.getCollisions()
+        else:
+            collisions = None
 
-        pressedKeys = pygame.key.get_pressed()
+        if self.isControllable:
+            pressedKeys = pygame.key.get_pressed()
 
-        if pressedKeys[K_a]:
-            self.acc.x = -self.acceleration
-        elif pressedKeys[K_d]:
-            self.acc.x = self.acceleration
+            if pressedKeys[K_a]:
+                self.acc.x = -self.acceleration
+            elif pressedKeys[K_d]:
+                self.acc.x = self.acceleration
 
-        if pressedKeys[K_SPACE]:
-            self.jump()
+            if pressedKeys[K_SPACE]:
+                self.jump()
 
         self.acc.x += self.vel.x * self.friction
         self.vel += self.acc
@@ -72,6 +80,20 @@ class Player(pygame.sprite.Sprite):
         if collisions:
             self.vel.y = self.jumpAmt
 
+    def kill(self):
+
+        if not self.isDead:
+            self.friction = 0
+            deathVel = 10
+            xVel = random.randint(-deathVel // 2, deathVel // 2)
+            yVel = -deathVel * 2
+            self.surf.fill((255, 0, 0))
+            self.vel = vec(xVel, yVel)
+            self.isDead = True
+            self.isControllable = False
+        else:
+            super().kill()
+
 class Platform(pygame.sprite.Sprite):
     def __init__(self, width, height, x, y):
         super().__init__()
@@ -80,3 +102,7 @@ class Platform(pygame.sprite.Sprite):
         self.surf = pygame.Surface((width, height))
         self.surf.fill((81, 95, 201))
         self.rect = self.surf.get_rect(center = (x, y))
+
+    #takes screen width and whether plat is odd in its group
+    def getRandX(self, isOdd, width):
+        return random.randint(isOdd * (width // 2), (width // 2) * (isOdd + 1))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 from classes import Player, Platform
  
 pygame.init()
@@ -8,13 +9,14 @@ vec = pygame.math.Vector2  # 2 for two dimensional
 HEIGHT = 900
 WIDTH = 800
 FPS = 60
+screenVel = 1
  
 FramePerSec = pygame.time.Clock()
  
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game")
 
-player = Player(30, 30, 30, HEIGHT - 50)
+player = Player(30, 30, WIDTH // 2, HEIGHT - 50)
 floor = Platform(WIDTH, 50, WIDTH / 2, HEIGHT - 25)
 
 allSprites = pygame.sprite.Group()
@@ -22,6 +24,19 @@ allSprites.add(player)
 allSprites.add(floor)
 platforms = pygame.sprite.Group()
 platforms.add(floor)
+respawnables = pygame.sprite.Group()
+
+for i in range(10):
+    height = HEIGHT / 10
+    odd = True
+    if i % 2 == 0:
+        odd = False
+    
+    platform = Platform(50, 25, 0, height * i)
+    platform.rect.centerx = platform.getRandX(odd, WIDTH)
+    platforms.add(platform)
+    allSprites.add(platform)
+    respawnables.add(platform)
 
 player.addCollisionGroup(platforms)
 
@@ -36,7 +51,24 @@ while True:
 
     displaysurface.fill((0,0,0))
 
+    #moving and respawning platforms
+    for i in range(len(platforms)):
+        sprites = platforms.sprites()
+        odd = True
+        if i % 2 == 0:
+            odd = False
+        sprites[i].rect = sprites[i].rect.move(0, screenVel)
+        #if sprites[i] is part of respawnables and off screen respawn it
+        if respawnables in sprites[i].groups() and sprites[i].rect.top >= HEIGHT:
+            newRect = sprites[i].surf.get_rect(center = (sprites[i].getRandX(odd, WIDTH), 0))
+            sprites[i].rect = newRect
+            
     player.move()
+
+    #handle player death
+    if player.rect.bottom >= HEIGHT:
+        player.rect.bottom = HEIGHT - 10
+        player.kill()
 
     for entity in allSprites:
         displaysurface.blit(entity.surf, entity.rect)
